8f34f2531e11749ef54820b26b9182dbab39ec4f
Carlos Rain <i@lovespite.com>
2024-02-04 14:05:07 +0800

    修改随机token算法

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,9 +1,24 @@
 let counter = 0;
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function getNextCount() {
   return counter++;
 }
 
 export default function getNextRandomToken() {
-  return getNextCount().toString(36);
+  return (
+    toBase62(Math.ceil(Math.random() * 0xffffffff)) +
+    getNextCount().toString(36) +
+    toBase62(Date.now())
+  );
+}
+
+function toBase62(num: number) {
+  const base = chars.length;
+  let result = "";
+  while (num > 0) {
+    result = chars[num % base] + result;
+    num = Math.floor(num / base);
+  }
+  return result || "0";
 }
